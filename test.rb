b8723cb4ddd3bdff63a12023a2f568e15f8814b1
John Kisor <jkisor@gmail.com>
2020-08-08 09:58:35 -0400

    fail when expected error never happens

--- test.rb
+++ test.rb
@@ -5,6 +5,8 @@ end
 def assert_error(&block)
   block.call
 rescue
+else
+  raise
 end
 
 # Successful tests succeed
@@ -12,5 +14,8 @@ assert(true)
 
 # Failing tests fail
 assert_error { assert(false) }
+
+# Fail when expect error never happens
+assert_error { assert_error { 1+1 } }
 
 puts "Success"
